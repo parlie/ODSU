5043d1fd49c1f35168bdee3257c021554b350bb4
_exception_ <zeuslets@gmail.com>
2020-10-04 21:53:25 +0200

    Second build, includes Generator class

---
+++ Generator.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+
+namespace ODSU
+{
+    public class Generator
+    {
+        private int dimensionX = 40;
+        private int dimensionY = 40;
+        
+        public Bitmap DrawODSU(string fileName, Tribit[] data)
+        {
+            Bitmap bitmap = new Bitmap(fileName);
+            Graphics g = Graphics.FromImage(bitmap);
+            Rectangle[] rectangles = new Rectangle[data.Length];
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (x < 800)
+                {
+                    Pen pen = new Pen(data[i].ToColor());
+                    g.DrawRectangle(pen,x,y,dimensionX,dimensionY);
+                    x = x + dimensionX;
+                }
+                else
+                {
+                    y = y + dimensionY;
+                    Pen pen = new Pen(data[i].ToColor());
+                    g.DrawRectangle(pen,x,y,dimensionX,dimensionY);
+                }
+            }
+            return bitmap;
+        }
+
+        public Bitmap DrawODSU(string fileName, int alternativeDimensionX, int alternativeDimensionY,Tribit[] data)
+        {
+            Bitmap bitmap = new Bitmap(fileName);
+            Graphics g = Graphics.FromImage(bitmap);
+            Rectangle[] rectangles = new Rectangle[data.Length];
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (x < 800)
+                {
+                    Pen pen = new Pen(data[i].ToColor());
+                    g.DrawRectangle(pen,x,y,alternativeDimensionX,alternativeDimensionY);
+                    x = x + alternativeDimensionX;
+                }
+                else
+                {
+                    y = y + dimensionY;
+                    Pen pen = new Pen(data[i].ToColor());
+                    g.DrawRectangle(pen,x,y,dimensionX,dimensionY);
+                }
+            }
+            return bitmap;
+        }
+    }
+}
